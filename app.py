6963a8ad0c41b6a2b0a1468fbaa11958df140cb5
irfanqs <irfanqobus.01@gmail.com>
2026-08-28 15:09:46 +0700

    feat: bind flask server to all interfaces (0.0.0.0)

--- app.py
+++ app.py
@@ -1320,4 +1320,4 @@ def file_too_large(_: Any) -> tuple[Any, int]:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", "5000")))
+    app.run(host="0.0.0.0", debug=True, port=int(os.environ.get("PORT", "5000")))
